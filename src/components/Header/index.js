8670388e5477f51a5fa797261b94752819d7d35b
sriniketh-reddy <122794477+sriniketh-reddy@users.noreply.github.com>
2024-07-13 16:26:38 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,19 +11,16 @@ const Header = () => (
       />
     </li>
     <li className="link-item">
-      Home
       <Link to="/" className="link">
         Home
       </Link>
     </li>
     <li className="link-item">
-      Products
       <Link to="/products" className="link">
         Products
       </Link>
     </li>
     <li className="link-item">
-      Cart
       <Link to="/cart" className="link">
         Cart
       </Link>
